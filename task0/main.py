913db2c5cc183c8796762513ecc5e493ece9b36a
michael <kiselev.0353@gmail.com>
2022-07-09 14:57:50 +0300

    redesign of xyz-coordinates calculation

--- task0/main.py
+++ task0/main.py
@@ -2,32 +2,53 @@ import open3d as o3d
 import numpy as np
 from PIL import Image
 
-extended_intrinsic_matrix = np.matrix([[481.20, 0, 319.50, 0],
-                                       [0, -480.00, 239.50, 0],
-                                       [0, 0, 1, 0],
-                                       [0, 0, 0, 1]])
 
-extendet_intrinsic_matrix_inverted = np.linalg.inv(extended_intrinsic_matrix)
+# Dataset and camera parameters.
+SCALE_FACTOR = 5000
 
-# converting a pixel to a pointcloud point
-def uvd2xyz(u, v, d):
-    z = d / 5000
-    xyz1 = z * extendet_intrinsic_matrix_inverted @ np.array([u, v, 1, 1 / z]).transpose()
-    return xyz1[0, 0], xyz1[0, 1], xyz1[0, 2]
+fx = 481.20
+fy = -480.00
+cx = 319.50
+cy = 239.50
 
-
-depths_img = Image.open('data/depth.png')
-colors_img = Image.open('data/annot.png')
+# 1. Creating the vector of colors.
+colors_image = Image.open('data/annot.png')
+colors_matrix = np.array(colors_image) / 256
+colors_vector = colors_matrix.reshape((307200, 3))
 
 
-points = [uvd2xyz(u, v, depths_img.getpixel((u, v)))
-          for u in range(depths_img.size[0]) for v in range(depths_img.size[1])]
-colors = [colors_img.getpixel((u, v))
-          for u in range(colors_img.size[0]) for v in range(colors_img.size[1])]
+# 2. Creating the vector of xyz-coordinates.
+# 2.0. Getting the depth matrix from the image.
+depths_image = Image.open('data/depth.png')
+depths_matrix = np.array(depths_image)
 
+# 2.1. Creating the vector of points where each element is (u_coordinate, v_coordinate, depth)
+M, N = depths_matrix.shape
+V_index, U_index = np.ix_(np.arange(M), np.arange(N))
+
+uvd_points_matrix = np.zeros(colors_matrix.shape)
+uvd_points_matrix[:, :, 0] = U_index
+uvd_points_matrix[:, :, 1] = V_index
+uvd_points_matrix[:, :, 2] = depths_matrix
+uvd_points_vector = uvd_points_matrix.reshape((uvd_points_matrix.shape[0] * uvd_points_matrix.shape[1], uvd_points_matrix.shape[2]))
+
+# 2.2. Separating the uvd-coordinates and calculating xyz-coordinates.
+U = uvd_points_vector[:, 0]
+V = uvd_points_vector[:, 1]
+D = uvd_points_vector[:, 2]
+
+Z = D / 5000
+X = (U - cx) * Z / fx
+Y = (V - cy) * Z / fy
+
+xyz_points_vector = np.column_stack((X, Y, Z))
+
+
+# 3. Visualizing the point cloud.
 pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(points)
-pcd.colors = o3d.utility.Vector3dVector(colors)
+pcd.points = o3d.utility.Vector3dVector(xyz_points_vector)
+pcd.colors = o3d.utility.Vector3dVector(colors_vector)
+
 
 pcd.transform([[-1, 0, 0, 0],
                [0, -1, 0, 0],
