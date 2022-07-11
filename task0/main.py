760c89283eb4da5212ef877f8fe801cb75478138
michael <kiselev.0353@gmail.com>
2022-07-11 15:08:07 +0300

    removed unnecessary operations

--- task0/main.py
+++ task0/main.py
@@ -19,47 +19,35 @@ def get_colors_vector(colors_image_path):
 
 
 def get_points_vector(depth_image_path):
-    # 0. Getting the depth matrix from the image.
     depths_image = Image.open(depth_image_path)
     depths_matrix = np.array(depths_image)
 
-    # 1. Creating the vector of points where each element is (u_coordinate, v_coordinate, depth)
     M, N = depths_matrix.shape
-    v_index, u_index = np.ix_(np.arange(M), np.arange(N))
+    V = np.repeat(np.arange(M), N)
+    U = np.tile(np.arange(N), M)
+    D = depths_matrix.reshape(depths_matrix.size)
 
-    uvd_points_matrix = np.zeros(colors_matrix.shape)
-    uvd_points_matrix[:, :, 0] = u_index
-    uvd_points_matrix[:, :, 1] = v_index
-    uvd_points_matrix[:, :, 2] = depths_matrix
-    uvd_points_vector = uvd_points_matrix.reshape((uvd_points_matrix.shape[0] * uvd_points_matrix.shape[1], uvd_points_matrix.shape[2]))
+    Z = D / 5000
+    X = (U - cx) * Z / fx
+    Y = (V - cy) * Z / fy
 
-    # 2. Separating the uvd-coordinates and calculating xyz-coordinates.
-    u_coordinates = uvd_points_vector[:, 0]
-    v_coordinates = uvd_points_vector[:, 1]
-    depths = uvd_points_vector[:, 2]
-
-    z_coordinates = depths / SCALE_FACTOR
-    x_coordinates = (u_coordinates - cx) * z_coordinates / fx
-    y_coordinates = (v_coordinates - cy) * z_coordinates / fy
-
-    xyz_points_vector = np.column_stack((x_coordinates, y_coordinates, z_coordinates))
+    xyz_points_vector = np.column_stack((X, Y, Z))
     return xyz_points_vector
 
 
-def visualize(colors_vector, points_vector):
+def visualize_pointcloud(points_vector, colors_vector):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points_vector)
     pcd.colors = o3d.utility.Vector3dVector(colors_vector)
 
 
     pcd.transform([[-1, 0, 0, 0],
-                [0, -1, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, -1]])
+                   [0, -1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, -1]])
     o3d.visualization.draw_geometries([pcd])
 
 
 if __name__ == '__main__':
-    colors_vector = get_colors_vector('data/annot.png')
-    points_vector = get_points_vector('data/depth.png')
-    visualize(colors_vector, points_vector)
+    visualize_pointcloud(get_points_vector('data/depth.png'),
+                         get_colors_vector('data/annot.png'))
